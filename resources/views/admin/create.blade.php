bc6c9ec855acc0799dbe0213b944b83f0abe7515
erictumuti <erictumuti114@gmail.com>
2020-07-31 14:53:42 +0300

    creating the template dashboard page and splitting the files in their respective section

---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,17 @@
+@extends('admin.layouts.master')
+
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                   oyaaaaaaaaaaaaaaaaaaaaaaaaaa
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
