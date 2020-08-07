65db36299dded00b5c9b3e43de9df85dd552a8d2
erictumuti <erictumuti114@gmail.com>
2020-08-07 09:27:42 +0300

    displaying the count number of users,notices,leaves and departments on the dashboard

--- resources/views/admin/layouts/content.blade.php
+++ resources/views/admin/layouts/content.blade.php
@@ -8,37 +8,53 @@
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Primary Card</div>
+                                    <div class="card-body">Users
+                                    <p><i class="fas fa-user fa-fw" style="font-size: 100px;"></i></p>
+                                    </div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <a class="small text-white stretched-link" href="#" style="font-size:18px;">
+                                        {{App\User::all()->count()}}
+                                        </a>
+                                        <div class="small text-white"></div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body">Warning Card</div>
+                                    <div class="card-body">Department
+                                    <p><i class="fas fa-home" style="font-size: 100px;"></i></p>
+                                    </div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <a class="small text-white stretched-link" href="#" style="font-size:18px;">
+                                        {{App\Department::all()->count()}}
+                                        </a>
+                                        <div class="small text-white"></div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Success Card</div>
+                                    <div class="card-body">Notice
+                                    <p><i class="fas fa-envelope" style="font-size: 100px;"></i></p>
+                                    </div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <a class="small text-white stretched-link" href="#" style="font-size:18px;">
+                                        {{App\Notice::all()->count()}}
+                                        </a>
+                                        <div class="small text-white"></div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body">Danger Card</div>
+                                    <div class="card-body">Leave
+                                    <p><i class="fas fa-book" style="font-size: 100px;"></i></p>
+                                    </div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <a class="small text-white stretched-link" href="#" style="font-size:18px;">
+                                        {{App\Leave::all()->count()}}
+                                        </a>
+                                        <div class="small text-white"></div>
                                     </div>
                                 </div>
                             </div>
